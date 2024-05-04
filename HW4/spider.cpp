3fefaf51a8670f89b749fc7b28cfd0c2bed8aa2c
Joanne <joanne.zh2015@gmail.com>
2024-05-04 20:54:22 +0800

    HW4: finish order

--- HW4/spider.cpp
+++ HW4/spider.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 class Edge{
 public:
-    int src, dest, dist, traffic;
+    int src, dest, dist, traffic, trafficnow = 0;
 
     Edge(int s, int d, int dis, int t): src(s), dest(d), dist(dis), traffic(t){};
 
@@ -13,21 +17,45 @@ public:
 class Vertex{
 public:
     int id, dman;
-    vector<Edge*> road;
+    unordered_map<int, Edge*> road;
 
     Vertex(int v, int c): id(v), dman(c){};
+};
+
+class Order{
+public:
+    vector<Edge*> path;
+    Vertex* driver;
+    int src, ts, minpath;
+
+    Order(int s, int t): src(s), ts(t){
+        minpath = INT_MAX;
+        path.clear();
+    }
+
+    void updateOrder(vector<Edge*> p, Vertex* d, int pa){
+        path = p;
+        driver = d;
+        minpath = pa;
+    }
 };
 
 class Map{
 public:
     vector<Vertex*> vertex;
     vector<Edge*> edges;
+    unordered_map<int, Order*> order;
 
+    Map(){};
     Map(int size){
         vertex.resize(size+1, nullptr);
         for(int i=1; i<=size; ++i){
             vertex[i] = new Vertex(i, -1);
         }
+    }
+
+    Vertex* operator[](int x){
+        return vertex[x];
     }
 
     void placeDman(int v, int c){
@@ -36,9 +64,16 @@ public:
 
     void addEdge(int s, int d, int dis, int t){
         Edge* newedge = new Edge(s, d, dis, t);
-        vertex[s]->road.push_back(newedge);
-        vertex[d]->road.push_back(newedge);
+        vertex[s]->road[d] = newedge;
+        vertex[d]->road[s] = newedge;
         edges.push_back(newedge);
+    }
+
+    void updateMap(vector<Edge*> path, int ts, Vertex* origin){
+        origin->dman--;
+        for(auto it : path){
+            it->trafficnow += ts;
+        }
     }
 };
 
@@ -47,7 +82,6 @@ int v, c, s, d, dis, t;
 string op;
 
 Map map;
-
 void initialMap(){
     map = Map(V);
     while(D--){
@@ -60,9 +94,49 @@ void initialMap(){
     }
 }
 
+vector<Edge*> minpath;
+Vertex* driverfrom;
+int mintemp = INT_MAX;
+void mindist(Vertex* src, Vertex* from, int dist, int ts, vector<Edge*> temppath){
+    if(src->dman > 0){
+        if(dist < mintemp || (dist == mintemp && temppath.size() < minpath.size())){
+            mintemp = dist;
+            minpath = temppath;
+            driverfrom = src;
+        }
+        return;
+    }
+
+    for(auto it : src->road){
+        Vertex* toward = map[it.first];
+        Edge* road = it.second;
+        int temp;
+        if(toward == from) continue;
+        if(road->traffic < road->trafficnow + ts) continue;
+
+        temppath.push_back(road);
+        mindist(toward, src, ts, dist + road->dist, temppath);
+        temppath.pop_back();
+    }
+}
+
 void order(){
     int id, src, ts;
     cin >> id >> src >> ts;
+
+    map.order[id] = new Order(src, ts);
+    vector<Edge*> vect;
+
+    mintemp = INT_MAX;
+    mindist(map[src], nullptr, 0, ts, vect);
+    
+    if(mintemp != INT_MAX){
+        map.order[id]->updateOrder(minpath, driverfrom, mintemp);
+        cout << "Order " << id << " from: " << driverfrom->id << endl;
+        map.updateMap(minpath, ts, driverfrom);
+    }else{
+        cout << "Just walk. T-T\n";
+    }
 }
 
 void drop(){
