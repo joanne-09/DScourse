142468aff8e0cf7549ad76e005b2ed46f37d2294
Joanne <joanne.zh2015@gmail.com>
2024-05-26 14:10:10 +0800

    HW5: finished

--- HW5/hospital.cpp
+++ HW5/hospital.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 using namespace std;
 
+using std::cout;
 class Patient{
 public:
     string name;
@@ -28,7 +29,7 @@ vector<Patient> Pandahospital;
 bool pandasort(Patient& left, Patient& right){
     if(left.membership != right.membership){
         return left.membership < right.membership;
-    }else if(left.date != right.date){
+    }else if(left.membership == 1 && left.date != right.date){
         return left.date > right.date;
     }else if(left.name != right.name){
         return left.name > right.name;
@@ -48,24 +49,31 @@ bool bearsort(Patient& left, Patient& right){
     return false;
 }
 
-void heapsort(vector<Patient>& hospital){
+long long heapsort(vector<Patient>& hospital, int size, int mode){
+    long long income = 0;
     for(int i=1; i<hospital.size(); ++i){
         int j = i;
-        while(j > 0 && FuncPtr(hospital[j], hospital[(j-1)/2])){
+        while(j > 0 && !FuncPtr(hospital[j], hospital[(j-1)/2])){
             swap(hospital[j], hospital[(j-1)/2]);
             j = (j-1)/2;
         }
     }
 
-    for(int i=hospital.size()-1; i>0; --i){
+    for(int i=hospital.size()-1; i>=0 && size>0; --i, --size){
+        if(mode){
+            income += hospital[0].money * 0.9;
+            cout << hospital[0].name << " " << hospital[0].age << endl;
+        }
+        
+        if(i == 0) break;
         swap(hospital[0], hospital[i]);
         int j = 0;
         while(2*j+1 < i){
             int k = 2*j+1;
-            if(k+1 < i && !FuncPtr(hospital[k], hospital[k+1])){
+            if(k+1 < i && FuncPtr(hospital[k], hospital[k+1])){
                 ++k;
             }
-            if(!FuncPtr(hospital[j], hospital[k])){
+            if(FuncPtr(hospital[j], hospital[k])){
                 swap(hospital[j], hospital[k]);
                 j = k;
             }else{
@@ -73,21 +81,14 @@ void heapsort(vector<Patient>& hospital){
             }
         }
     }
-}
-
-long long output(vector<Patient>& hospital){
-    long long income = 0;
-    for(auto& it : hospital){
-        income += it.money * 0.9;
-        cout << it.name << " " << it.age << endl;
-        //cout << it.money << " " << it.membership << endl;
-    }
 
     return income;
 }
 
 int main(){
     cin >> N >> M;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     while(N--){
         cin.ignore();
         getline(cin, nam);
@@ -108,27 +109,28 @@ int main(){
             ++idx;
         }
 
+        cout << "DAY #" << i << endl;
+        long long income = 0;
         FuncPtr = pandasort;
-        heapsort(treatlist);
-
-        if(treatlist.size() < X){
-            Pandahospital.assign(treatlist.begin(), treatlist.end());
+        
+        if(treatlist.size() <= X){
+            income = heapsort(treatlist, treatlist.size(), 1);
             treatlist.clear();
         }else{
-            Pandahospital.assign(treatlist.begin(), treatlist.begin()+X);
-            treatlist.erase(treatlist.begin(), treatlist.begin()+X);
+            income = heapsort(treatlist, X, 1);
+            treatlist.erase(treatlist.begin()+(treatlist.size()-X), treatlist.end());
 
-            if(treatlist.size() < K){
+            if(treatlist.size() <= K){
                 treatlist.clear();
-            }else{
+            }else if(K != 0){
                 FuncPtr = bearsort;
-                heapsort(treatlist);
-                treatlist.erase(treatlist.begin(), treatlist.begin()+K);
+                heapsort(treatlist, K, 0);
+                treatlist.erase(treatlist.begin()+(treatlist.size()-K), treatlist.end());
             }
         }
 
-        cout << "DAY #" << i << endl;
-        long long income = output(Pandahospital);
+        //cout << "DAY #" << i << endl;
+        //long long income = output(Pandahospital);
         cout << "INCOME TODAY: " << income << endl;
     }
     return 0;
