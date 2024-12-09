c65b72b4a483ad028bcf43e19ffede3eb439c757
joanne-09 <132663938+joanne-09@users.noreply.github.com>
2024-12-09 23:00:03 +0800

    Update quiz5.cpp

--- quiz5/quiz5.cpp
+++ quiz5/quiz5.cpp
@@ -67,3 +67,124 @@ int main(){
     cout << cnt << endl;
     return 0;
 }
+
+// by copilot
+
+// SOLUTION: using merge sort
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// long long inversion_count = 0;
+
+// void merge(vector<int>& arr, int left, int mid, int right) {
+//     int n1 = mid - left + 1;
+//     int n2 = right - mid;
+
+//     // Temporary arrays
+//     vector<int> L(n1);
+//     vector<int> R(n2);
+
+//     // Copy data to temp arrays
+//     for(int i = 0; i < n1; ++i)
+//         L[i] = arr[left + i];
+//     for(int j = 0; j < n2; ++j)
+//         R[j] = arr[mid + 1 + j];
+
+//     // Merge the temp arrays back into arr[left..right]
+//     int i = 0; // Initial index of first subarray
+//     int j = 0; // Initial index of second subarray
+//     int k = left; // Initial index of merged subarray
+
+//     while(i < n1 && j < n2) {
+//         if(L[i] <= R[j]) {
+//             arr[k++] = L[i++];
+//         } else {
+//             arr[k++] = R[j++];
+//             inversion_count += n1 - i; // Count inversions
+//         }
+//     }
+
+//     // Copy the remaining elements of L[], if any
+//     while(i < n1)
+//         arr[k++] = L[i++];
+
+//     // Copy the remaining elements of R[], if any
+//     while(j < n2)
+//         arr[k++] = R[j++];
+// }
+
+// void mergeSort(vector<int>& arr, int left, int right) {
+//     if(left >= right)
+//         return;
+    
+//     int mid = left + (right - left) / 2;
+//     mergeSort(arr, left, mid);       // Sort first half
+//     mergeSort(arr, mid + 1, right);  // Sort second half
+//     merge(arr, left, mid, right);    // Merge the sorted halves
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int n;
+//     cin >> n;
+//     vector<int> p(n);
+//     for(int i = 0; i < n; ++i)
+//         cin >> p[i];
+    
+//     inversion_count = 0;
+//     mergeSort(p, 0, n - 1);
+
+//     cout << inversion_count << '\n';
+//     return 0;
+// }
+
+// SOLUTION: using BIT tree
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Fenwick Tree Implementation for Inversion Count
+// class BIT{
+//     int n;
+//     vector<int> bit;
+// public:
+//     void init(int _n){
+//         n = _n;
+//         bit.assign(n + 1, 0);
+//     }
+//     void update(int x, int val){
+//         for(; x <= n; x += x & -x)
+//             bit[x] += val;
+//     }
+//     int query(int x) const {
+//         int sum = 0;
+//         for(; x > 0; x -= x & -x)
+//             sum += bit[x];
+//         return sum;
+//     }
+// };
+
+// int main(){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int n;
+//     cin >> n;
+//     vector<int> p(n);
+//     for(int i = 0; i < n; ++i)
+//         cin >> p[i];
+
+//     // Initialize BIT with size equal to the maximum value in p
+//     BIT tree;
+//     tree.init(n);
+
+//     long long inversion_count = 0;
+//     for(int i = n - 1; i >= 0; --i){
+//         inversion_count += tree.query(p[i] - 1); // Number of elements less than p[i]
+//         tree.update(p[i], 1); // Increase count of p[i]
+//     }
+
+//     cout << inversion_count << '\n';
+//     return 0;
+// }
